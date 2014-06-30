fb39d45cf79512d50fdeeb48757cfea28190ee26
zykor <enrique.wx@gmail.com>
2014-06-30 17:58:14 -0500

    Primer commit

---
+++ pide_informacion.py
@@ -0,0 +1,17 @@
+TRUE = [True, "si", "Si", "s", "S", 1, "Yes", "yes", "Y", "y", "true"]
+
+def solicita_string(mensaje):
+	res = raw_input(mensaje)
+	return res
+
+def solicita_int(mensaje):
+	res = raw_input(mensaje)
+	if res.isdigit():
+		return int(res)
+	return None
+
+def solicita_si_no(mensaje):
+	res = raw_input(mensaje)
+	if res in true:
+    	return True
+    return False
